0128852d0d4776f610b753dd506fbe5ede0d03c8
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-10-03 21:13:20 +0300

    some styles + caption

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ import { Box, Container, Typography } from '@mui/material'
 
 export default function Welcome() {
     const m700 = useMediaQuery("(max-width: 700px)")
-    console.log(m700)
+
     return (
         <Box 
             sx={{
@@ -27,19 +27,22 @@ export default function Welcome() {
                 justifyContent: "center",
                 width: "100%",
                 height: "200px",
-                borderRadius: "24px",
+                borderRadius: m700 ? "0" : "24px",
                 backdropFilter: "blur(12px)",
                 color: "#fff",
                 textAlign: "center"
             }}>
-                <Typography variant={m700 ? 'h5' : 'h4'} component='h2'>
-                    Hi, there! I'm Zaur, 17 yo, Moscow student
+                <Typography variant={m700 ? 'h5' : 'h4'} component='h2' mt="auto">
+                    Hi there. I'm Zaur, 17 yo, Moscow student
                 </Typography>
                 <Typography variant="subtitle2" component="span">and also</Typography>
                 <Typography variant={m700 ? 'h6' : 'h5'} component="p">
                     <Typography color="#35b4b0" variant={m700 ? 'h6' : 'h5'} component="span">
                         Ambitious
                     </Typography> FrontEnd developer
+                </Typography>
+                <Typography variant="caption" sx={{mt: "auto"}}>
+                    Design isn't my strong suit, so don't evaluate strictly)
                 </Typography>
             </Container>
         </Box>
